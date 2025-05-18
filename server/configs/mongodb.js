f8d49c7789c894b5ba3c1741681596bf65dd5c47
Samrat Ghosh <legendsamratpool89@gmail.com>
2025-05-18 11:47:26 +0530

    Update mongodb.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-// Connect to MongoDB
+// Connect to the MongoDB database
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => {
-        console.log('MongoDB connected');
-    });
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+
+    mongoose.connection.on('connected', () => console.log('Database Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+
 }
 
-
-export default connectDB;
+export default connectDB
